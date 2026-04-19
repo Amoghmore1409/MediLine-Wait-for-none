d2fe0cf38de1a4dc808121843349dec4fd0e7fad
Amoghmore1409 <165577768+Amoghmore1409@users.noreply.github.com>
2026-04-20 00:44:12 +0530

    Made changes to make repo public

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.google.gms.google.services)
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
     namespace = "com.example.mediline"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.mediline"
